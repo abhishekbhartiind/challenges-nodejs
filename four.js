b0569a25199d99db63356bb3ea70be6a439a9302
abhishekbhartiind <imabhishekbharti12@gmail.com>
2023-02-28 02:55:47 +0530

    minor updates

--- four.js
+++ four.js
@@ -1,32 +1,34 @@
 const pro = {
-    name: 'abhishek',
-    getArrowName: () => {
-        console.log(this.name)
-    },
-    getName: function(){
-        console.log(this.name)
-    }
+  name: "abhishek",
+  getArrowName: () => {
+    console.log(this.name, this)
+  },
+  getName: function () {
+    console.log(this.name, this)
+  },
 }
 
 pro.getName() //abhishek
+pro.getArrowName()
 //undefined pro.gerArrowName
 
 //---------------------------------
-let x = [1, 2,2 ,4, 2,4]
+let x = [1, 2, 2, 4, 2, 4]
 let maxNum = Math.max(...x)
-let acc = 0;
-for(let i=0; i< x.length;i++){
-    if(maxNum === x[i]){
-        acc = acc+1
-    }
+let acc = 0
+for (let i = 0; i < x.length; i++) {
+  if (maxNum === x[i]) {
+    acc = acc + 1
+  }
 }
 
 console.log(acc)
 
-
-var s = "#hallo, this is a test #john #doe";
-var re = /(?:^|\W)#(\w+)(?!\w)/g, match, matches = [];
-while (match = re.exec(s)) {
-  matches.push(match[1]);
+var s = "#hallo, this is a test #john #doe"
+var re = /(?:^|\W)#(\w+)(?!\w)/g,
+  match,
+  matches = []
+while ((match = re.exec(s))) {
+  matches.push(match[1])
 }
-console.log(matches);
+console.log(matches)
--- three.js
+++ three.js
@@ -1,27 +1,27 @@
 const val = {
-    penny:1,
-    nickel:5,
-    dime:10,
-    quater:25,
-    dollar:100
+  penny: 1,
+  nickel: 5,
+  dime: 10,
+  quater: 25,
+  dollar: 100,
 }
 //20.47 -bring every thing in one unit
 
-function money(total){
-    let cenT = total*100;
-    let re = cenT%val.dollar
-    let dollar = cenT -re
-    return dollar
+function money(total) {
+  let cenT = total * 100
+  let re = cenT % val.dollar
+  let dollar = cenT - re
+  return dollar
 }
 console.log(money(20.47))
 
 // --------------------------------
-let str = 'i love javascript'
-console.log(str.split('').reverse().join(''))
+let str = "i love javascript"
+console.log(str.split("").reverse().join(""))
 
 //--------------------------------
 
-const ary = [1,2,3,4]
+const ary = [1, 2, 3, 4]
 
 /* 
 1 - 2+3+4(Max)
@@ -31,26 +31,27 @@ const ary = [1,2,3,4]
 */
 
 function findMaxMin(array) {
-    let min = Math.min.apply(null, array)
-    let max = Math.max.apply(null, array)
+  let min = Math.min(...array)
+  //Math.min.apply(null, array)
+  let max = Math.max(...array)
+  //Math.max.apply(null, array)
 
-    let arrSum = array.reduce((acc, item) => acc + item)
-    let minSum = arrSum - max
-    let maxSum = arrSum - min
-    return `Max: ${maxSum} \t Min: ${minSum}`
+  let arrSum = array.reduce((acc, item) => acc + item)
+  let minSum = arrSum - max
+  let maxSum = arrSum - min
+  return `Max: ${maxSum} \t Min: ${minSum}`
 }
 console.log(findMaxMin(ary))
-
 
 let x = 1234
 // find length of number
 
-function len(number){
-    let numLength = 0;
-    while(number != 0){
-        number = Math.floor(number / 10)
-        numLength++
-    }
-    return numLength
+function len(number) {
+  let numLength = 0
+  while (number != 0) {
+    number = Math.floor(number / 10)
+    numLength++
+  }
+  return numLength
 }
 console.log(len(x))
